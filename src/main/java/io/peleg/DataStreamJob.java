f19cc3b007b3b2f47158ef1e1828017b0955a100
Peleg Tsadok <pelegtsa@gmail.com>
2022-12-23 15:20:57 +0200

    Removed imports

--- src/main/java/io/peleg/DataStreamJob.java
+++ src/main/java/io/peleg/DataStreamJob.java
@@ -2,15 +2,7 @@ package io.peleg;
 
 import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.api.common.time.Time;
-import org.apache.flink.configuration.RestartStrategyOptions;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.api.common.serialization.SimpleStringEncoder;
-import org.apache.flink.core.fs.Path;
-import org.apache.flink.configuration.MemorySize;
-import org.apache.flink.connector.file.sink.FileSink;
-import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy;
-
-import java.time.Duration;
 
 public class DataStreamJob {
 
