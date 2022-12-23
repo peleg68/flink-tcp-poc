ab7997d93bfd4aa5aed88ece69c891ed195a22c7
Peleg Tsadok <pelegtsa@gmail.com>
2022-12-23 12:47:21 +0200

    Changed tcpserver to keep listening

--- src/main/python/tcpserver.py
+++ src/main/python/tcpserver.py
@@ -11,32 +11,37 @@ args = sys.argv[1:]
 HOST = ''
 PORT = int(args[0])
 
-logging.info(f'Opening server on host {HOST} and port {PORT}.')
-
-# Create a TCP socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Bind the socket to the address and port
-server_socket.bind((HOST, PORT))
-
-# Listen for incoming connections
-server_socket.listen()
-
-logging.info(f'Started listening on host {HOST} and port {PORT}.')
-
-# Accept a connection
-client_socket, client_address = server_socket.accept()
-
-logging.info(f'Accepted connection on address {client_address}.')
-
 while True:
-    # Generate a random number between 1 and 666
-    num = str(random.randint(1, 666))
+    logging.info(f'Opening server on host {HOST} and port {PORT}.')
 
-    # Send the random number as a string to the client
-    client_socket.sendall(num.encode())
+    # Create a TCP socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen()
 
-    # Wait for 1 second before sending the next random number
-    time.sleep(1)
+    logging.info(f'Started listening on host {HOST} and port {PORT}.')
+
+    client_socket, client_address = server_socket.accept()
+
+    logging.info(f'Accepted connection on address {client_address}.')
+
+    while True:
+        # Generate a random number between 1 and 666
+        num = str(random.randint(1, 666))
+
+        try:
+            # Send the random number as a string to the client
+            client_socket.sendall(num.encode())
+        except:
+            logging.info(f'Connection CLOSED on address {client_address}.')
+            break
+
+        # Wait for 1 second before sending the next random number
+        time.sleep(1)
+
+    logging.info(f'Releasing connection resources of {client_address}.')
+
+    client_socket.close()
+    server_socket.close()
 
 logging.info(f'Finished')
