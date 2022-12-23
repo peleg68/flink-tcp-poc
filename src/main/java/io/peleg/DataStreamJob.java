e954f9529a7bfe57d0b185887102c9393d260183
Peleg Tsadok <pelegtsa@gmail.com>
2022-12-23 11:51:26 +0200

    Added filter and file sink

--- src/main/java/io/peleg/DataStreamJob.java
+++ src/main/java/io/peleg/DataStreamJob.java
@@ -1,6 +1,13 @@
 package io.peleg;
 
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.api.common.serialization.SimpleStringEncoder;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.configuration.MemorySize;
+import org.apache.flink.connector.file.sink.FileSink;
+import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy;
+
+import java.time.Duration;
 
 public class DataStreamJob {
 
@@ -8,21 +15,34 @@ public class DataStreamJob {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         String[] servers = {
-            "localhost",
-            "localhost",
-            "localhost",
-            "localhost",
+                "localhost",
+                "localhost",
+                "localhost",
+                "localhost",
         };
 
         int[] ports = {
-            8001,
-            8002,
-            8003,
-            8004,
+                8001,
+                8002,
+                8003,
+                8004,
         };
 
+        String outputPath = "./out"
+
+        final FileSink<String> sink = FileSink
+                .forRowFormat(new Path(outputPath), new SimpleStringEncoder<String>("UTF-8"))
+                .withRollingPolicy(
+                        DefaultRollingPolicy.builder()
+                                .withRolloverInterval(Duration.ofMinutes(15))
+                                .withInactivityInterval(Duration.ofMinutes(5))
+                                .withMaxPartSize(MemorySize.ofMebiBytes(1024))
+                                .build())
+                .build();
+
         env.addSource(new TcpSource(servers, ports))
-                        .print("print-sink");
+                .filter(s -> Integer.parseInt(s) % 3)
+                .addSink(sink);
 
         env.execute("tcp-poc");
     }
