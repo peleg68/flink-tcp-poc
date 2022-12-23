5d707ed0f964186042d9aef5b905c332cdb4d542
Peleg Tsadok <pelegtsa@gmail.com>
2022-12-23 10:58:29 +0200

    Written basic DataStreamJob

--- src/main/java/io/peleg/DataStreamJob.java
+++ src/main/java/io/peleg/DataStreamJob.java
@@ -1,4 +1,12 @@
 package io.peleg;
 
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
 public class DataStreamJob {
+
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+
+        env.execute("tcp-poc");
 }
