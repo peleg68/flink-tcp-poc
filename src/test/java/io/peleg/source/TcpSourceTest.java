97bd8c29be637f497da56328de9d91970d3a8ab1
Peleg Tsadok <pelegtsa@gmail.com>
2022-12-24 11:54:13 +0200

    Started writing unit tests

---
+++ src/test/java/io/peleg/source/TcpSourceTest.java
@@ -0,0 +1,38 @@
+package io.peleg.source;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TcpSourceTest {
+    TcpSource tcpSource;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getBoundedness() {
+    }
+
+    @Test
+    void createReader() {
+    }
+
+    @Test
+    void createEnumerator() {
+    }
+
+    @Test
+    void restoreEnumerator() {
+    }
+
+    @Test
+    void getSplitSerializer() {
+    }
+
+    @Test
+    void getEnumeratorCheckpointSerializer() {
+    }
+}
