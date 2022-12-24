cc2b481ce3783e8f0af49f3c9d46d979e8f88e7d
Peleg Tsadok <pelegtsa@gmail.com>
2022-12-24 13:01:24 +0200

    Implemented TcpServerSplit data class

--- src/main/java/io/peleg/source/TcpServerSplit.java
+++ src/main/java/io/peleg/source/TcpServerSplit.java
@@ -1,10 +1,17 @@
 package io.peleg.source;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.apache.flink.api.connector.source.SourceSplit;
 
+@Data
+@AllArgsConstructor
 public class TcpServerSplit implements SourceSplit {
+    private final String host;
+    private final int port;
+
     @Override
     public String splitId() {
-        return null;
+        return host + ":" + port;
     }
 }
