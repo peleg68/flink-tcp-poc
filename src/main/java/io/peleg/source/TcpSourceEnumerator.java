2d864c19fcc22c6a7d1711461fa7ff7a185a3d75
Peleg Tsadok <pelegtsa@gmail.com>
2022-12-25 22:25:06 +0200

    Added some more basic stuff to enumerator

--- src/main/java/io/peleg/source/TcpSourceEnumerator.java
+++ src/main/java/io/peleg/source/TcpSourceEnumerator.java
@@ -1,12 +1,18 @@
 package io.peleg.source;
 
+import lombok.AllArgsConstructor;
+import org.apache.flink.api.connector.source.SourceSplit;
 import org.apache.flink.api.connector.source.SplitEnumerator;
+import org.apache.flink.api.connector.source.SplitEnumeratorContext;
+import org.apache.flink.api.connector.source.SplitsAssignment;
 
 import javax.annotation.Nullable;
 import java.io.IOException;
 import java.util.List;
 
+@AllArgsConstructor
 public class TcpSourceEnumerator implements SplitEnumerator<TcpServerSplit, TcpSourceState> {
+    private final SplitEnumeratorContext<TcpServerSplit> splitEnumeratorContext;
 
     @Override
     public void start() {
@@ -23,6 +29,10 @@ public class TcpSourceEnumerator implements SplitEnumerator<TcpServerSplit, TcpS
 
     }
 
+    /**
+     * Should assign given reader to pending splits using {@link SplitEnumeratorContext#assignSplit(SourceSplit, int)} or {@link SplitEnumeratorContext#assignSplits(SplitsAssignment)}.
+     * @param i
+     */
     @Override
     public void addReader(int i) {
 
